e9cb4c6b07ccb15f2f056a169b7d20d55135202d
anuragpandey1rkt-cmyk <anuragpandey1rkt@gmail.com>
2026-03-11 19:24:00 +0530

    swagger

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify, send_from_directory
 from config import Config
 from models import db
 from auth import auth
 from notes import notes
 from flask_jwt_extended import JWTManager
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,6 +14,20 @@ jwt = JWTManager(app)
 
 app.register_blueprint(auth)
 app.register_blueprint(notes)
+
+SWAGGER_URL = "/docs"
+API_URL = "/swagger.json"
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={"app_name": "Notes Management API"}
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
+
+@app.route("/swagger.json")
+def swagger_spec():
+    return send_from_directory(".", "swagger.json")
 
 with app.app_context():
     db.create_all()
